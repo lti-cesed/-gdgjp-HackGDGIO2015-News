839791a52468d167627a51fd636c7aea1391cb5d
buenonp <buenonp@gmail.com>
2015-05-30 14:44:45 -0300

    script polymer

--- js/notificacaoController.js
+++ js/notificacaoController.js
@@ -1,12 +1,37 @@
-var app = angular.module('NewsWear', []);
+Polymer('music-demo', {
+      page: 0,
+      notificacaos: [
 
-app.controller('NotificacaosController', ['$scope', function($scope) {
+            {"titulo": "Assalto ao Banco Bradesco em Campina Grande",
+                      "mensagem": "Sete homens armados com fuzis chegaram em uma agência do Bradesco próxima a feira ..."},
+            {"titulo": "Teste",
+              "mensagem": "Testando ..."}
 
-    $scope.notificacaos = [
-        {"titulo": "Assalto ao Banco Bradesco em Campina Grande",
-          "mensagem": "Sete homens armados com fuzis chegaram em uma agência do Bradesco próxima a feira ..."},
-        {"titulo": "Rafael",
-          "mensagem": "Testando ..."}
-    ];
-
-}]);
+      ],
+      selectedAlbum: null,
+      transition: function(e) {
+        if (this.page === 0 && e.target.templateInstance.model.item) {
+          this.selectedAlbum = e.target.templateInstance.model.item;
+          this.page = 1;
+        }
+        else {
+          this.page = 0;
+        }
+      },
+      updateElements: function() {
+          var request = new XMLHttpRequest();
+          request.onreadystatechange = function (event) {
+              if (request.readyState === 4) {
+                  if (request.status === 200) {
+                      var resposta = JSON.parse(this.responseText);
+                      document.querySelector('music-demo').items = resposta;
+                  } else {
+                      console.log = ("Não foi possivel recuperar dados");
+                  }
+                  request.abort();
+              }
+          };
+          request.open('get', 'https://webservicecartpol.herokuapp.com/server/bo', true);
+          request.send();
+      }
+    });
